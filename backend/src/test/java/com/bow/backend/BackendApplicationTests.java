706ec37be3569f4c1df384c76d1d8bb8012572af
WWjinpeng <695297734@qq.com>
2024-10-20 23:42:49 +0800

    Mysql配置登录与注册

--- backend/src/test/java/com/bow/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/bow/backend/BackendApplicationTests.java
@@ -2,12 +2,19 @@ package com.bow.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("wjp1"));
+        System.out.println(passwordEncoder.encode("wjp2"));
+//        System.out.println(passwordEncoder.encode("wjp3"));
+//        System.out.println(passwordEncoder.encode("wjp4"));
     }
 
 }
